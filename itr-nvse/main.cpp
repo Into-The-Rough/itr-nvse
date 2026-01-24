a6a28554adb954a67f85a82ed0d2f1fc74ef9148
IntoTheRough <john@intotherough.co.uk>
2026-01-24 21:10:06 +0000

    extract KillActorXPFix from main.cpp

--- itr-nvse/main.cpp
+++ itr-nvse/main.cpp
@@ -47,6 +47,7 @@ __forceinline T_Ret CdeclCall(uint32_t _addr, Args ...args) {
 #include "PlayerUpdateHook.h"
 #include "SlowMotionPhysicsFix.h"
 #include "VATSProjectileFix.h"
+#include "KillActorXPFix.h"
 
 #include <cstdio>
 #include <cstring>
@@ -566,65 +567,6 @@ static CommandInfo kCommandInfo_GetNoWeaponSearch = {
 	1, 0, nullptr, Cmd_GetNoWeaponSearch_Execute, nullptr, nullptr, 0
 };
 
-namespace KillActorXPFix
-{
-	constexpr uint32_t kAddr_XPBlockStart = 0x5BE379;
-	constexpr uint32_t kAddr_XPBlockEnd = 0x5BE3FA;
-	constexpr uint32_t kAddr_ActorGetLevel = 0x87F9F0;
-	constexpr uint32_t kAddr_ReturnAfterHook = 0x5BE381;
-	constexpr uint32_t kOffset_Actor_LifeState = 0x108;
-
-	void PatchWrite8(uint32_t addr, uint8_t data) {
-		DWORD oldProtect;
-		VirtualProtect((void*)addr, 1, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*(uint8_t*)addr = data;
-		VirtualProtect((void*)addr, 1, oldProtect, &oldProtect);
-	}
-
-	void PatchWrite32(uint32_t addr, uint32_t data) {
-		DWORD oldProtect;
-		VirtualProtect((void*)addr, 4, PAGE_EXECUTE_READWRITE, &oldProtect);
-		*(uint32_t*)addr = data;
-		VirtualProtect((void*)addr, 4, oldProtect, &oldProtect);
-	}
-
-	void WriteRelJump(uint32_t src, uint32_t dst) {
-		PatchWrite8(src, 0xE9);  // jmp rel32
-		PatchWrite32(src + 1, dst - src - 5);
-	}
-
-	__declspec(naked) void Hook_XPBlockStart()
-	{
-		__asm
-		{
-			mov ecx, [ebp-0x10]
-			mov eax, [ecx + 0x108] //lifeState: 0=alive,1=dying,2=dead
-			cmp eax, 1
-			je skip_xp
-			cmp eax, 2
-			je skip_xp
-
-			mov eax, kAddr_ActorGetLevel
-			call eax
-			mov eax, kAddr_ReturnAfterHook
-			jmp eax
-
-		skip_xp:
-			mov eax, kAddr_XPBlockEnd
-			jmp eax
-		}
-	}
-
-	void Init()
-	{
-		WriteRelJump(kAddr_XPBlockStart, (uint32_t)Hook_XPBlockStart);
-		PatchWrite8(kAddr_XPBlockStart + 5, 0x90);
-		PatchWrite8(kAddr_XPBlockStart + 6, 0x90);
-		PatchWrite8(kAddr_XPBlockStart + 7, 0x90);
-		Log("KillActorXPFix installed");
-	}
-}
-
 namespace ReversePickpocketNoKarmaFix
 {
 	constexpr uint32_t kAddr_TryPickpocket = 0x75E0B0;
@@ -1092,7 +1034,7 @@ static void MessageHandler(NVSEMessagingInterface::Message* msg)
 				if (Settings::bExplodingPantsFix)
 					ExplodingPantsFix::Init();
 				if (Settings::bKillActorXPFix)
-					KillActorXPFix::Init();
+					KillActorXPFix_Init();
 				if (Settings::bReversePickpocketNoKarma)
 					ReversePickpocketNoKarmaFix::Init();
 				if (Settings::bSaveFileSize)
