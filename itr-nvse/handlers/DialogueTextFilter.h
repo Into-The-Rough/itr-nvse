b38e4db65a7f53d377f6b8a9302ecc9075bf2161
IntoTheRough <john@intotherough.co.uk>
2026-02-02 17:55:55 +0000

    validate form pointers in dialogue callback
    prevents crash when speaker/topicInfo become invalid during cell transitions

--- itr-nvse/handlers/DialogueTextFilter.h
+++ itr-nvse/handlers/DialogueTextFilter.h
@@ -2,8 +2,21 @@
 
 struct NVSEInterface_DTF;
 struct Script_DTF;
+class Actor;
+class TESTopicInfo;
+class TESTopic;
 
 bool DTF_Init(void* nvseInterface);
 bool DTF_AddFilter(const char* filterText, void* callback);
 bool DTF_RemoveFilter(const char* filterText, void* callback);
 unsigned int DTF_GetOpcode();
+
+//native callback type for other plugins
+//duration is in seconds, calculated from text length * fNoticeTextTimePerCharacter
+typedef void (*DTF_NativeCallback)(Actor* speaker, const char* text, float duration, TESTopicInfo* topicInfo, TESTopic* topic);
+
+//exported functions for inter-plugin communication
+extern "C" {
+__declspec(dllexport) bool DTF_RegisterNativeCallback(DTF_NativeCallback callback);
+__declspec(dllexport) bool DTF_UnregisterNativeCallback(DTF_NativeCallback callback);
+}
