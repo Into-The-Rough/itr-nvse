956b6a35acd54a2bafe6eddd0bd9a3318375c511
IntoTheRough <john@intotherough.co.uk>
2026-02-09 14:53:46 +0000

    require exact runtime version match for hardcoded addresses

--- itr-nvse/main.cpp
+++ itr-nvse/main.cpp
@@ -13,7 +13,8 @@ __declspec(dllexport) bool NVSEPlugin_Query(const NVSEInterface* nvse, PluginInf
 
 	if (nvse->isEditor) return true;
 	if (nvse->nvseVersion < NVSE_VERSION_INTEGER) return false;
-	if (nvse->runtimeVersion < RUNTIME_VERSION_1_4_0_525) return false;
+	// hardcoded engine addresses require exact runtime match
+	if (nvse->runtimeVersion != RUNTIME_VERSION_1_4_0_525) return false;
 	if (nvse->isNogore) return false;
 
 	return true;
