a1c094bb11bee5f37282b15bcecb6f1f35ab7e1a
IntoTheRough <john@intotherough.co.uk>
2026-04-08 21:14:54 +0100

    refactor FallDamageHandler state helpers

--- itr-nvse/handlers/FallDamageHandler.h
+++ itr-nvse/handlers/FallDamageHandler.h
@@ -1,8 +1,14 @@
 #pragma once
 
+class Actor;
+
 namespace FallDamageHandler {
 	bool Init(void* nvse);
 	void RegisterCommands(void* nvse);
 	UInt32 GetSetMultOpcode();
 	UInt32 GetGetMultOpcode();
+	bool HasOverride(Actor* actor);
+	void SetMultiplier(float mult, Actor* actor = nullptr);
+	float GetMultiplier(Actor* actor = nullptr);
+	void ClearMultiplier(Actor* actor = nullptr);
 }
